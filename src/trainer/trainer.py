84bb4d54867e4e10efbd384c75b0b4dd03fb0c1f
drAlexAK <alex.a.kudryashov@gmail.com>
2026-05-17 09:40:06 +0300

    fix: discriminator logic in train pipeline and checkpoints

--- src/trainer/trainer.py
+++ src/trainer/trainer.py
@@ -1,4 +1,5 @@
 import torch
+from tqdm.auto import tqdm
 
 from src.metrics.tracker import MetricTracker
 from src.trainer.base_trainer import BaseTrainer
@@ -180,3 +181,206 @@ class Trainer(BaseTrainer):
                 batch["result"][i],
                 sample_rate=self.sample_rate,
             )
+
+    def _save_checkpoint(self, epoch, save_best=False, only_best=False):
+        """
+        Save the checkpoints.
+
+        Args:
+            epoch (int): current epoch number.
+            save_best (bool): if True, rename the saved checkpoint to 'model_best.pth'.
+            only_best (bool): if True and the checkpoint is the best, save it only as
+                'model_best.pth'(do not duplicate the checkpoint as
+                checkpoint-epochEpochNumber.pth)
+        """
+        arch = type(self.model).__name__
+        state = {
+            "arch": arch,
+            "epoch": epoch,
+            "state_dict": self.model.state_dict(),
+            "discriminators": self.discriminators.state_dict(),
+            "optimizer": self.optimizer.state_dict(),
+            "discriminator_optimizer": self.discriminator_optimizer.state_dict(),
+            "lr_scheduler": self.lr_scheduler.state_dict(),
+            "discriminator_lr_scheduler": (
+                self.discriminator_lr_scheduler.state_dict()
+            ),
+            "monitor_best": self.mnt_best,
+            "config": self.config,
+        }
+        filename = str(self.checkpoint_dir / f"checkpoint-epoch{epoch}.pth")
+        if not (only_best and save_best):
+            torch.save(state, filename)
+            if self.config.writer.log_checkpoints:
+                self.writer.add_checkpoint(filename, str(self.checkpoint_dir.parent))
+            self.logger.info(f"Saving checkpoint: {filename} ...")
+        if save_best:
+            best_path = str(self.checkpoint_dir / "model_best.pth")
+            torch.save(state, best_path)
+            if self.config.writer.log_checkpoints:
+                self.writer.add_checkpoint(best_path, str(self.checkpoint_dir.parent))
+            self.logger.info("Saving current best: model_best.pth ...")
+
+    def _resume_checkpoint(self, resume_path):
+        """
+        Resume from a saved checkpoint (in case of server crash, etc.).
+        The function loads state dicts for everything, including model,
+        optimizers, etc.
+
+        Notice that the checkpoint should be located in the current experiment
+        saved directory (where all checkpoints are saved in '_save_checkpoint').
+
+        Args:
+            resume_path (str): Path to the checkpoint to be resumed.
+        """
+        resume_path = str(resume_path)
+        self.logger.info(f"Loading checkpoint: {resume_path} ...")
+        checkpoint = torch.load(resume_path, self.device)
+        self.start_epoch = checkpoint["epoch"] + 1
+        self.mnt_best = checkpoint["monitor_best"]
+
+        # load architecture params from checkpoint.
+        if checkpoint["config"]["model"] != self.config["model"]:
+            self.logger.warning(
+                "Warning: Architecture configuration given in the config file is different from that "
+                "of the checkpoint. This may yield an exception when state_dict is loaded."
+            )
+        self.model.load_state_dict(checkpoint["state_dict"])
+        self.discriminators.load_state_dict(checkpoint["discriminators"])
+
+        # load optimizer state from checkpoint only when optimizer type is not changed.
+        if (
+            checkpoint["config"]["generator_optimizer"]
+            != self.config["generator_optimizer"]
+            or checkpoint["config"]["generator_lr_scheduler"]
+            != self.config["generator_lr_scheduler"]
+        ):
+            self.logger.warning(
+                "Warning: Generator optimizer or lr_scheduler given in the config file is different "
+                "from that of the checkpoint. Optimizer and scheduler parameters "
+                "are not resumed."
+            )
+        else:
+            self.optimizer.load_state_dict(checkpoint["optimizer"])
+            self.lr_scheduler.load_state_dict(checkpoint["lr_scheduler"])
+
+        if (
+            checkpoint["config"]["discriminator_optimizer"]
+            != self.config["discriminator_optimizer"]
+            or checkpoint["config"]["discriminator_lr_scheduler"]
+            != self.config["discriminator_lr_scheduler"]
+        ):
+            self.logger.warning(
+                "Warning: Discriminator optimizer or lr_scheduler given in the config file is different "
+                "from that of the checkpoint. Discriminator optimizer and scheduler parameters "
+                "are not resumed."
+            )
+        else:
+            self.discriminator_optimizer.load_state_dict(
+                checkpoint["discriminator_optimizer"]
+            )
+            self.discriminator_lr_scheduler.load_state_dict(
+                checkpoint["discriminator_lr_scheduler"]
+            )
+
+        self.logger.info(
+            f"Checkpoint loaded. Resume training from epoch {self.start_epoch}"
+        )
+
+    def _train_epoch(self, epoch):
+        """
+        Training logic for an epoch, including logging and evaluation on
+        non-train partitions.
+
+        Args:
+            epoch (int): current training epoch.
+        Returns:
+            logs (dict): logs that contain the average loss and metric in
+                this epoch.
+        """
+        self.is_train = True
+        self.model.train()
+        self.discriminators.train()
+        self.train_metrics.reset()
+        self.writer.set_step((epoch - 1) * self.epoch_len)
+        self.writer.add_scalar("epoch", epoch)
+        for batch_idx, batch in enumerate(
+            tqdm(self.train_dataloader, desc="train", total=self.epoch_len)
+        ):
+            try:
+                batch = self.process_batch(
+                    batch,
+                    metrics=self.train_metrics,
+                )
+            except torch.cuda.OutOfMemoryError as e:
+                if self.skip_oom:
+                    self.logger.warning("OOM on batch. Skipping batch.")
+                    torch.cuda.empty_cache()  # free some memory
+                    continue
+                else:
+                    raise e
+
+            self.train_metrics.update("grad_norm", self._get_grad_norm())
+
+            # log current results
+            if batch_idx % self.log_step == 0:
+                self.writer.set_step((epoch - 1) * self.epoch_len + batch_idx)
+                self.logger.debug(
+                    "Train Epoch: {} {} Loss: {:.6f}".format(
+                        epoch, self._progress(batch_idx), batch["loss"].item()
+                    )
+                )
+                if self.lr_scheduler is not None:
+                    self.writer.add_scalar(
+                        "learning rate", self.lr_scheduler.get_last_lr()[0]
+                    )
+                self._log_scalars(self.train_metrics)
+                self._log_batch(batch_idx, batch)
+                # we don't want to reset train metrics at the start of every epoch
+                # because we are interested in recent train metrics
+                last_train_metrics = self.train_metrics.result()
+                self.train_metrics.reset()
+            if batch_idx + 1 >= self.epoch_len:
+                break
+
+        logs = last_train_metrics
+
+        # Run val/test
+        for part, dataloader in self.evaluation_dataloaders.items():
+            val_logs = self._evaluation_epoch(epoch, part, dataloader)
+            logs.update(**{f"{part}_{name}": value for name, value in val_logs.items()})
+
+        return logs
+
+    def _evaluation_epoch(self, epoch, part, dataloader):
+        """
+        Evaluate model on the partition after training for an epoch.
+
+        Args:
+            epoch (int): current training epoch.
+            part (str): partition to evaluate on
+            dataloader (DataLoader): dataloader for the partition.
+        Returns:
+            logs (dict): logs that contain the information about evaluation.
+        """
+        self.is_train = False
+        self.model.eval()
+        self.discriminators.eval()
+        self.evaluation_metrics.reset()
+        with torch.no_grad():
+            for batch_idx, batch in tqdm(
+                enumerate(dataloader),
+                desc=part,
+                total=len(dataloader),
+            ):
+                batch = self.process_batch(
+                    batch,
+                    metrics=self.evaluation_metrics,
+                )
+            self.writer.set_step(epoch * self.epoch_len, part)
+            self._log_scalars(self.evaluation_metrics)
+            self._log_batch(
+                batch_idx, batch, part
+            )  # log only the last batch during inference
+
+        return self.evaluation_metrics.result()
